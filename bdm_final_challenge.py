0c31c44ccf5dd2b35ed688db206223712a47a962
Betholdcamp <54646677+Betholdcamp@users.noreply.github.com>
2020-05-18 11:33:57 -0400

    Update bdm_final_challenge.py

--- bdm_final_challenge.py
+++ bdm_final_challenge.py
@@ -6,7 +6,7 @@ import re
 if __name__ == '__main__':
     sc = SparkContext()
     streets = sc.textFile('/data/share/bdm/nyc_cscl.csv', use_unicode=False).cache() #'/data/share/bdm/complaints.csv'
-    all_tickets = sc.textFile('/data/share/bdm/nyc_parking_violation/', use_unicode=False).cache()
+    all_tickets = sc.textFile('/data/share/bdm/nyc_parking_violation/2015.csv,/data/share/bdm/nyc_parking_violation/2016.csv,/data/share/bdm/nyc_parking_violation/2017.csv,/data/share/bdm/nyc_parking_violation/2018.csv,/data/share/bdm/nyc_parking_violation/2019.csv', use_unicode=False).cache()
     
     def lines(partId, records):
         if partId==0:
@@ -19,25 +19,29 @@ if __name__ == '__main__':
                 (PHYSICALID, L_LOW_HN, L_HIGH_HN, R_LOW_HN, R_HIGH_HN, ST_LABEL, BOROCODE, FULL_STREE) = (row[0], row[2], row[3], row[4], row[5], row[10], row[13], row[28])
                 L_low1 = re.findall(r'\d+',L_LOW_HN)
                 L_low = list(map(int, L_low1))
+                L_low = tuple(L_low)
                 
                 L_high1 = re.findall(r'\d+',L_HIGH_HN)
                 L_high = list(map(int, L_high1))
+                L_high = tuple(L_high)
                 
                 R_low1 = re.findall(r'\d+',R_LOW_HN)
                 R_low = list(map(int, R_low1))
+                R_low = tuple(R_low)
                 
                 R_high1 = re.findall(r'\d+',R_HIGH_HN)
                 R_high = list(map(int, R_high1))
+                R_high = tuple(R_high)
                 
                 borocode = ('Unknown', 'NY' , 'BX', 'K', 'Q', 'R')
                 yield (int(PHYSICALID), (L_low, L_high), (R_low, R_high), ST_LABEL, borocode[int(BOROCODE)], FULL_STREE)
     
     street_line = streets.mapPartitionsWithIndex(lines)
-    street_list = street_line.collect()
+    street_list = sc.broadcast(street_line.collect())
     
     def findid(borough, street, h_num):
         dd = None
-        for i in street_list:
+        for i in street_list.value:
             if (i[3] == street or i[5] == street) and i[4]==borough:
                 if (len(h_num) == 1) and (h_num[0] % 2 == 0) and (h_num[0] >= i[2][0][0] and h_num[0] <= i[2][1][0]):
                     dd = i[0]
@@ -50,7 +54,10 @@ if __name__ == '__main__':
                     break  
                 elif (len(h_num) == 2) and (h_num[1] % 2 != 0) and (len(i[1][0])==2) and (len(i[1][1])==2) and (h_num[1] >= i[1][0][1] and h_num[1] <= i[1][1][1]):
                     dd = i[0]
-                    break 
+                    break
+                else:
+                    dd = None
+                    break
             else:
                 dd = None
                 break
@@ -64,12 +71,14 @@ if __name__ == '__main__':
         for row in reader:
             if row[4] != '' and row[21] != '' and row[23] != '' and row[24] != '':
                 (date, county, house_number, street_name, ) = (row[4], row[21], row[23], row[24])
-                d = int(date.split('/')[2])
+                #d = int(date.split('/')[2])
+                d = date.split('/')
                 temp = re.findall(r'\d+',house_number)
                 res = list(map(int,temp))
-                if len(res) > 0:
+                res = tuple(res)
+                if len(res) > 0 and len(d) > 2:
                     idd = findid(county, street_name, res)
-                    if idd != None and (d >= 2015 and d <= 2019):
+                    if idd != None and (int(d[2]) >= 2015 and int(d[2]) <= 2019):
                         yield (idd, d)
     
     ticket = all_tickets.mapPartitionsWithIndex(extractScores)
